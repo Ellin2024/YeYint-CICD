d734312a8d6d32ce98f05a5c6754862eb0aac499
Ellin2024 <ellinyumiko@gmail.com>
2025-11-11 11:43:02 +0630

    Update WelcomeController.java

--- src/main/java/com/yym/controller/WelcomeController.java
+++ src/main/java/com/yym/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController 
 {
-	@GetMapping("/Welcome")
+	@GetMapping("/index")
 	public String welcomePage()
 	{
 		String text = "Welcome to Docker";
